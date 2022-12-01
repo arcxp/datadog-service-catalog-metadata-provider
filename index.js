e278ef2ea58ff6034a6f2e883b223f52b6171617
Michael D. Stemle, Jr <mike.stemle@washpost.com>
2022-12-01 17:07:47 -0500

    Trying to get anything to work with this silly thing.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-module.exports = async function () {
-  try {
-    const configs = [
-      'datadog-key',
-      'service-name',
-      'team',
-      'email',
-      'slack-support-channel',
-      'repo',
-      'contacts',
-      'repos',
-      'tags',
-      'links',
-      'docs',
-    ].reduce((value) => {
-      const config = core.getInput(value)
-      return config ? { ...configs, [value]: config } : configs
-    }, {})
+fdsj(
 
-    console.log('All of the configs:', configs)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+try {
+  const configs = [
+    'datadog-key',
+    'service-name',
+    'team',
+    'email',
+    'slack-support-channel',
+    'repo',
+    'contacts',
+    'repos',
+    'tags',
+    'links',
+    'docs',
+  ].reduce((value) => {
+    const config = core.getInput(value)
+    return config ? { ...configs, [value]: config } : configs
+  }, {})
+
+  core.info('All of the configs:', configs)
+} catch (error) {
+  core.setFailed(error.message)
 }
