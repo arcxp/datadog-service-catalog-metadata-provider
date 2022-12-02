b3598af790e935893ccd6ca948292e804443f35d
Michael D. Stemle, Jr <mike.stemle@washpost.com>
2022-12-02 13:11:32 -0500

    Adding the expansion of YAML parameters stashed as strings.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const YAML = require('yaml')
+
+/**
+ * This function takes inputs encoded as strings and expands them to
+ * the full array of objects.
+ * @param {string} str - The input value.
+ * @returns {Object} The input value with all object inputs expanded.
+ **/
+const expandObjectInputs = (str) => {
+  try {
+    return YAML.parse(str)
+  } catch (error) {
+    core.debug(`Input as <<${str}>> is not a valid YAML object.`)
+    core.error(error)
+    core.setFailed(error.message)
+  }
+}
 
 try {
   const configs = [
@@ -16,7 +33,7 @@ try {
     'docs',
   ].reduce((agg, inputName) => {
     const inputValue = core.getInput(inputName)
-    return Object.assign(agg, { [inputName]: inputValue })
+    return Object.assign(agg, { [inputName]: expandObjectInputs(inputValue) })
   }, {})
 
   core.debug(`All of the configs: ${JSON.stringify(configs, undefined, 2)}`)
