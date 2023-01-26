feb83e2c1511df395097fd04f66fc1056f5afa8e
Michael D. Stemle, Jr <mike.stemle@washpost.com>
2023-01-25 20:43:56 -0500

    Adding the datadog host parameter.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { inputsToRegistryDocument } = require('./lib/input-to-registry-document')
  * DataDog.
  * @param {string} config - The config JSON string.
  **/
-const registerWithDataDog = async (apiKey, appKey, configJsonStr) => {
+const registerWithDataDog = async (apiKey, appKey, ddHost, configJsonStr) => {
   core.debug(`JSON: ${configJsonStr}`)
   // Prep the auth
   const client = new HttpClient(
@@ -18,7 +18,7 @@ const registerWithDataDog = async (apiKey, appKey, configJsonStr) => {
   )
 
   const response = await client.post(
-    'https://api.datadoghq.com/api/v2/services/definitions',
+    `https://${ddHost}/api/v2/services/definitions`,
     configJsonStr,
     {
       'DD-API-KEY': apiKey,
@@ -43,14 +43,24 @@ const run = async (configs) => {
     const apiKey = core.getInput('datadog-key')
     const appKey = core.getInput('datadog-app-key')
     if (!apiKey || !appKey) {
-      return core.setFailed('Both `datadog-key` and `datadog-app-key` are required.')
+      return core.setFailed(
+        'Both `datadog-key` and `datadog-app-key` are required.',
+      )
+    }
+
+    // Fetch and verify the host.
+    const ddHost = core.getInput('datadog-host')
+    if (!ddHost || !ddHost.match(/^[a-z0-9.-]+\.datadoghq\.(com|eu|us)$/)) {
+      return core.setFailed(
+        `Invalid DataDog host: ${ddHost}. See here for more details: https://docs.datadoghq.com/getting_started/site/`,
+      )
     }
 
     // Debug all of the info
     core.debug(`All of the configs: ${JSON.stringify(configs, undefined, 2)}`)
 
     // Register the service with DataDog
-    await registerWithDataDog(apiKey, appKey, JSON.stringify(configs))
+    await registerWithDataDog(apiKey, appKey, ddHost, JSON.stringify(configs))
   } catch (error) {
     console.error(error)
     core.setFailed(error.message)
