eab5f78be1ccff23987a5d6b120c9f6d46a5aaee
Michael D. Stemle, Jr <mike.stemle@washpost.com>
2023-02-14 11:17:01 -0500

    Adding test to print debug info. Just exploring Actions.

--- __tests__/lib/org-rules.test.js
+++ __tests__/lib/org-rules.test.js
@@ -1,5 +1,8 @@
+const { applyOrgRules } = require('../../lib/org-rules')
+
 describe('org-rules.js', () => {
   test('#getOrgRules() - normal input', async () => {
+    applyOrgRules()
     expect(1).toBeTruthy()
   })
 })
