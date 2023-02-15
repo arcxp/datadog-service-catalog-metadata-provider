059bb2852f499a3c954f8255c00817206aa839bc
Michael D. Stemle, Jr <mike.stemle@washpost.com>
2023-02-15 12:40:17 -0500

    Got contexts working within automated tests. Yay.

---
+++ __mocks__/@actions/github.js
@@ -0,0 +1,7 @@
+const github = require('@actions/github')
+
+console.debug(
+  '!i!i!i!i!i!i ---- @actions/github mock is loaded ---- !i!i!i!i!i!i',
+)
+
+module.exports = github
--- __tests__/lib/org-rules.test.js
+++ __tests__/lib/org-rules.test.js
@@ -1,8 +1,21 @@
+// Seed these for GitHub's toolkit
+const path = require('path')
+process.env.GITHUB_EVENT_PATH = path.join(
+  __dirname,
+  '../data/github-context-payload.json',
+)
+process.env.GITHUB_REPOSITORY =
+  'arcxp/datadog-service-catalog-metadata-provider'
+
+// Pulling this in here activates the mocking of the github module
+const github = require('@actions/github')
+
+// This is our test subject
 const { applyOrgRules } = require('../../lib/org-rules')
 
 describe('org-rules.js', () => {
   test('#getOrgRules() - normal input', async () => {
-    applyOrgRules()
+    await applyOrgRules()
     expect(1).toBeTruthy()
   })
 })
