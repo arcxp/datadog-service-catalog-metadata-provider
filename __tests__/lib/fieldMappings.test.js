1516f223c07c0929a31a209c2ff70d2e83d1cf8b
Michael D. Stemle, Jr <mike.stemle@washpost.com>
2023-06-19 16:07:05 +0000

    Got a bunch of field mappings done.

--- __tests__/lib/fieldMappings.test.js
+++ __tests__/lib/fieldMappings.test.js
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview This test covers all of the field mappings across versions.
+ **/
+
 const { mappings, mapField } = require('../../lib/fieldMappings')
 
 describe.each([
@@ -37,20 +41,140 @@ describe.each([
     input: 'app-team-name',
     value: { team: 'app-team-name' },
   },
-  // STOPPED HERE
   {
     version: 'v2',
     field: 'contacts',
     input: `
-name: Testy McTester
-type: email
-contact: testy@mctester.com
+- name: Testy McTester
+  type: email
+  contact: testy@mctester.com
 `,
-    value: { contacts: '' },
+    value: {
+      contacts: [
+        {
+          name: 'Testy McTester',
+          type: 'email',
+          contact: 'testy@mctester.com',
+        },
+      ],
+    },
+  },
+  {
+    version: 'v2.1',
+    field: 'contacts',
+    input: `
+- name: Testy McTester
+  type: email
+  contact: testy@mctester.com
+`,
+    value: {
+      contacts: [
+        {
+          name: 'Testy McTester',
+          type: 'email',
+          contact: 'testy@mctester.com',
+        },
+      ],
+    },
+  },
+  {
+    version: 'v2',
+    field: 'tags',
+    input: `
+- plain:nospaces
+- compat1: space-in-value
+- compat2 : space-in-both
+`,
+    value: {
+      tags: [
+        'plain:nospaces',
+        'compat1:space-in-value',
+        'compat2:space-in-both',
+      ],
+    },
+  },
+  {
+    version: 'v2.1',
+    field: 'tags',
+    input: `
+- plain:nospaces
+- compat1: space-in-value
+- compat2 : space-in-both
+`,
+    value: {
+      tags: [
+        'plain:nospaces',
+        'compat1:space-in-value',
+        'compat2:space-in-both',
+      ],
+    },
+  },
+  {
+    version: 'v2',
+    field: 'links',
+    input: `
+- name: 'first-link'
+  type: url
+  url: https://manchicken.io/testing
+`,
+    value: {
+      links: [
+        {
+          name: 'first-link',
+          type: 'url',
+          url: 'https://manchicken.io/testing',
+        },
+      ],
+    },
+  },
+  {
+    version: 'v2',
+    field: 'links',
+    input: `
+- name: 'first-link'
+  type: url
+  url: https://manchicken.io/testing
+  provider: Website
+`,
+    value: {
+      links: [
+        {
+          name: 'first-link',
+          type: 'url',
+          url: 'https://manchicken.io/testing',
+        },
+      ],
+    },
+  },
+  {
+    version: 'v2.1',
+    field: 'links',
+    input: `
+- name: 'first-link'
+  type: url
+  url: https://manchicken.io/testing
+  provider: Website
+`,
+    value: {
+      links: [
+        {
+          name: 'first-link',
+          type: 'url',
+          url: 'https://manchicken.io/testing',
+          provider: 'Website',
+        },
+      ],
+    },
   },
 ])('$field:$version', ({ version, field, input, value }) => {
   test('mapping', () => {
-    console.debug({ result: mapField(field, version)(input), value })
+    console.debug(
+      JSON.stringify(
+        { result: mapField(field, version)(input), value },
+        undefined,
+        2,
+      ),
+    )
     expect(mapField(field, version)(input)).toEqual(value)
   })
 })
